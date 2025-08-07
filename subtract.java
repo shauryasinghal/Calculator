4dd428e4820f619506ad6a8177425dea4b774288
SHAURYA SINGHAL <shauryasinghal150@gmail.com>
2025-08-07 10:57:09 +0530

    Add subtract.java to subtraction branch

---
+++ subtract.java
@@ -0,0 +1,9 @@
+public class SubtractNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+        int difference = a - b;
+        System.out.println("Difference: " + difference);
+    }
+}
+
