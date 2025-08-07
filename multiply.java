c18e7911eb17b23f9b6bf01e2c4b59f8961cae72
SHAURYA SINGHAL <shauryasinghal150@gmail.com>
2025-08-07 10:59:11 +0530

    Add multiply.java to multiplication branch

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class MultiplyNumbers {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 7;
+        int product = a * b;
+        System.out.println("Product: " + product);
+    }
+}
+
