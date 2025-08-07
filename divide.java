7e1cbaf335ac0e210d00a3896a9123c03ca2644c
SHAURYA SINGHAL <shauryasinghal150@gmail.com>
2025-08-07 11:00:33 +0530

    Add divide.java to division branch

---
+++ divide.java
@@ -0,0 +1,9 @@
+public class DivideNumbers {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 4;
+        int result = a / b;
+        System.out.println("Quotient: " + result);
+    }
+}
+
