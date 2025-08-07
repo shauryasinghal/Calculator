3875abc0189d2d33850f10b7281ce636057cda1d
SHAURYA SINGHAL <shauryasinghal150@gmail.com>
2025-08-07 10:46:13 +0530

    Add add.java to addidtion branch

---
+++ add.java
@@ -0,0 +1,9 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
+
